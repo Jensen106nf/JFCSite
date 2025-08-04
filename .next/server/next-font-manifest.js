a4c4c8fa19a5ea5a589ae0e9946792f573c41d93
Brian Jensen <BrianJensen@MacBook-Air.local>
2025-08-04 13:42:41 -0500

    Add .gitignore and clean up repository

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/BrianJensen/Desktop/Jensen Floorcare/Jensen Floorcare Site/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7LolBAn1B7v4KAhkwpef3nE3D1AJxBvlg2zaGnMs6ac=\"\n}"
